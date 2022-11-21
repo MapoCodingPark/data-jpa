05cb4d70a139f5e073a62de511912979a590b74d
MapoCodingPark <parkpkww@gmail.com>
2022-11-22 02:23:34 +0900

    2022/11/22
    - section 1 복습

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat; // static으로 assert
 
 @SpringBootTest // spring container : spring bean 다 injection 받아서 쓸거임
 @Transactional // jpa 의 모든 data 변경은 transaction 에서 일어나야함
-@Rollback(false) // test 끝나고 rollback 하는거 방지 > test에서 일어난 db 변화 등을 볼 수 있고, console log로도 보임 _ 실무에서는 없어야지/**/
+@Rollback(false) // test 끝나고 rollback 하는거 방지 > test에서 일어난 db 변화 등을 볼 수 있고 (h2 console 가보면 남아있음), console log로도 보임 _ 실무에서는 없어야지/**/
 class MemberJpaRepositoryTest {
 
     @Autowired
